220ea7e84e0c2647eca238bd11622744d7f81892
Cooper Werner <cooperwerner824@gmail.com>
2023-12-03 14:36:17 -0500

    Adding GUI for controlling the rover

--- src/control/laptopClient.py
+++ src/control/laptopClient.py
@@ -1,4 +1,7 @@
+# path: src/control/laptopClient.py
+
 import bluetooth
+import tkinter as tk
 
 client_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
@@ -10,24 +13,46 @@ for i, device in enumerate(devices):
 # connect to device
 device = int(input("Select device: "))
 bluetooth_address = devices[device-1]
-
 port = 1
 
 client_socket.connect((bluetooth_address, port))
 
+def dataSmartSend(client_socket, data) -> bool:
+    try:
+        client_socket.send(data)
+        return True
+    except bluetooth.btcommon.BluetoothError as err:
+        print("Bluetooth Error: {}".format(err))
+        return False
+
 while True:
     try:
-        data = input()
-        if len(data) == 0:
-            break
-        
-        try:
-            client_socket.send(data)
-        except bluetooth.btcommon.BluetoothError as err:
-            print("Bluetooth Error: {}".format(err))
-            break
+        # tk window to send actions for the rover
+        window = tk.Tk()
+        window.title("Rover Control")
+        window.geometry("200x200")
+        window.configure(background="grey")
+
+        # add 4 buttons to the window
+        forward = tk.Button(window, text="Forward", command=lambda: dataSmartSend(client_socket, "F"))
+        forward.pack(side=tk.TOP)
+        left = tk.Button(window, text="Left", command=lambda: dataSmartSend(client_socket, "L"))
+        left.pack(side=tk.LEFT)
+        right = tk.Button(window, text="Right", command=lambda: dataSmartSend(client_socket, "R"))
+        right.pack(side=tk.RIGHT)
+        backward = tk.Button(window, text="Backward", command=lambda: dataSmartSend(client_socket, "B"))
+        backward.pack(side=tk.BOTTOM)
+
+        window.mainloop()
+
+        # close the socket when the window is closed
+        client_socket.close()
+        break
 
     except(KeyboardInterrupt, SystemExit):
         print("Closing socket")
         client_socket.close()
         break
+
+
+
