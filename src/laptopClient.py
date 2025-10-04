d8a47c827565c94551ef8b40a59ce027c2e47b1e
atciamb <ciamba@rpi.edu>
2025-10-04 18:48:41 -0400

    Started OOP

--- src/laptopClient.py
+++ src/laptopClient.py
@@ -1,8 +1,27 @@
 from ControlClient.MainWindow import MainWindow
-from PySide6 import QtCore, QtWidgets, QtGui
+#from PySide6 import QtCore, QtWidgets, QtGui, QLabel
 import sys
+from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QLabel, QPushButton, QComboBox, QLineEdit)
+from PySide6.QtGui import QPixmap, QFont
+from PySide6.QtCore import Qt, QTimer
 
-# Hello World
+class Rover_Cont_Pan(QMainWindow):
+    def init(self):
+        super.__init__()
+        self.centWidg=QWidget()
+        self.setWindowTitle("Rover Control Panel")
+        self.setCentralWidget(self.centWidg)
+        self.main_layout = QGridLayout(self.central_widget)
+
+        # Quad 1:Rover control
+        #Need to add actual image
+
+        self.Q1=QWidget()
+        self.Q1_layout=QVBoxLayout(self.Q1)
+        self.main_layout.addWidget(self.quadrant1_widget,0,0)
+
+
+
 
 def main(argc: int, argv: list[str]) -> int:
     app = QtWidgets.QApplication(argv)
@@ -16,5 +35,6 @@ def main(argc: int, argv: list[str]) -> int:
     return app.exec()
 
 if __name__ == "__main__":
-    sys.exit(main(len(sys.argv), sys.argv))
+    #sys.exit(main(len(sys.argv), sys.argv))
+
     
