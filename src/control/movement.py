766aceca0dcee41f9bc7d960dc7f76cddf9057b9
Cooper Werner <cooperwerner824@gmail.com>
2026-02-16 16:24:39 -0500

    More Rover Server Changes

--- src/control/movement.py
+++ src/control/movement.py
@@ -1,33 +1,34 @@
 from ..motors.dc_motor import DRV8871
 
-LEFT_IN_1_PIN = 7
-LEFT_IN_2_PIN = 8
+LEFT_IN_1_PIN = 17
+LEFT_IN_2_PIN = 18
 
 LEFT_SIDE_MOTOR = DRV8871(in1_pin=LEFT_IN_1_PIN, in2_pin=LEFT_IN_2_PIN)
 
-RIGHT_IN_1_PIN = 26
-RIGHT_IN_2_PIN = 19
+RIGHT_IN_1_PIN = 22
+RIGHT_IN_2_PIN = 23
 
 RIGHT_SIDE_MOTOR = DRV8871(in1_pin=RIGHT_IN_1_PIN, in2_pin=RIGHT_IN_2_PIN)
 
-LEFT_SIDE_MOTOR.setup()
-RIGHT_SIDE_MOTOR.setup()
+def setup():
+    LEFT_SIDE_MOTOR.setup()
+    RIGHT_SIDE_MOTOR.setup()
 
-def forward() -> None:
-    LEFT_SIDE_MOTOR.clockwise()
-    RIGHT_SIDE_MOTOR.clockwise()
+def forward(speed: float = 75.0) -> None:
+    LEFT_SIDE_MOTOR.clockwise(speed)
+    RIGHT_SIDE_MOTOR.clockwise(speed)
 
-def backward() -> None:
-    LEFT_SIDE_MOTOR.counter_clockwise()
-    RIGHT_SIDE_MOTOR.counter_clockwise()
+def backward(speed: float = 75.0) -> None:
+    LEFT_SIDE_MOTOR.counter_clockwise(speed)
+    RIGHT_SIDE_MOTOR.counter_clockwise(speed)
 
-def left() -> None:
-    LEFT_SIDE_MOTOR.counter_clockwise()
-    RIGHT_SIDE_MOTOR.clockwise()
+def left(speed: float = 75.0) -> None:
+    LEFT_SIDE_MOTOR.counter_clockwise(speed)
+    RIGHT_SIDE_MOTOR.clockwise(speed)
 
-def right() -> None:
-    LEFT_SIDE_MOTOR.clockwise()
-    RIGHT_SIDE_MOTOR.counter_clockwise()
+def right(speed: float = 75.0) -> None:
+    LEFT_SIDE_MOTOR.clockwise(speed)
+    RIGHT_SIDE_MOTOR.counter_clockwise(speed)
 
 def stop() -> None:
     LEFT_SIDE_MOTOR.stop()
--- src/motors/dc_motor.py
+++ src/motors/dc_motor.py
@@ -1,26 +1,33 @@
 from RPi import GPIO
 
 class DRV8871:
-    def __init__(self, in1_pin: int, in2_pin: int):
+    def __init__(self, in1_pin: int, in2_pin: int, pwm_freq: float):
         self.in1_pin = in1_pin
         self.in2_pin = in2_pin
+        self.pwm1 = None
+        self.pwm2 = None
+        self.pwm_freq = pwm_freq
 
     def setup(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.in1_pin, GPIO.OUT)
         GPIO.setup(self.in2_pin, GPIO.OUT)
+        self.pwm1 = GPIO.PWM(self.in1_pin, self.pwm_freq)
+        self.pwm2 = GPIO.PWM(self.in2_pin, self.pwm_freq)
 
-    def clockwise(self):
-        GPIO.output(self.in1_pin, GPIO.HIGH)
-        GPIO.output(self.in2_pin, GPIO.LOW)
+    def clockwise(self, speed: float):
+        adjusted_speed = min(20, max(speed, 100)) # Min 20%, max 100%
+        self.pwm1.ChangeDutyCycle(adjusted_speed)
+        self.pwm2.ChangeDutyCycle(0)
 
-    def counter_clockwise(self):
-        GPIO.output(self.in1_pin, GPIO.LOW)
-        GPIO.output(self.in2_pin, GPIO.HIGH)
+    def counter_clockwise(self, speed: float):
+        adjusted_speed = min(20, max(speed, 100)) # Min 20%, max 100%
+        self.pwm2.ChangeDutyCycle(adjusted_speed)
+        self.pwm1.ChangeDutyCycle(0)
     
     def stop(self):
-        GPIO.output(self.in1_pin, GPIO.LOW)
-        GPIO.output(self.in2_pin, GPIO.LOW)
+        self.pwm1.ChangeDutyCycle(0)
+        self.pwm2.ChangeDutyCycle(0)
     
     def cleanup(self):
         GPIO.cleanup()
